b28029ae95315aeb02fba95e12ab5b2b06223985
Terry <accoutwork1201@gmail.com>
2022-12-05 14:26:32 +0800

    add button updatetextView and viewBinding

--- app/src/main/java/com/example/git_source/MainActivity.java
+++ app/src/main/java/com/example/git_source/MainActivity.java
@@ -3,12 +3,25 @@ package com.example.git_source;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.example.git_source.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+
+        click();
+    }
+
+    private void click(){
+        binding.textResult.setText("Click button");
     }
 }
